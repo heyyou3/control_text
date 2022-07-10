2cb2bed475e49b2ebf82c323e589e46e385d13a1
heyyou <hey3you3@gmail.com>
2022-07-11 00:27:30 +0900

    feat(adapter/input): Implements Ktor API

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,13 +6,19 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+val ktorVersion = "2.0.2"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-gson:$ktorVersion")
 }
 
 tasks.test {
---
+++ src/main/kotlin/adapter/input/api/ktor/Main.kt
@@ -0,0 +1,51 @@
+package adapter.input.api.ktor
+
+import adapter.output.text.StdOutTextAdapter
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import port.reverseText.input.ReverseTextInput
+import port.reverseText.output.ReverseTextOutput
+import port.reverseText.usecase.ReverseTextUseCase
+import shared.port.Code
+import java.text.DateFormat
+
+data class ReverseTextResponse(val text: String)
+data class ReverseTextErrorResponse(val message: String)
+
+fun main(args: Array<String>) {
+    val server = embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            gson {
+                setDateFormat(DateFormat.LONG)
+                setPrettyPrinting()
+            }
+        }
+        routing {
+            get("/api/reverse_text") {
+                val reqText = call.request.queryParameters["text"]
+                if (reqText == null) {
+                    call.respond(HttpStatusCode.BadRequest, ReverseTextErrorResponse("required text query parameter"))
+                    return@get
+                }
+                val textInput = ReverseTextInput(ReverseTextUseCase(ReverseTextOutput(StdOutTextAdapter())))
+                val res = textInput.invoke(reqText)
+                val reverseText = res.data ?: ""
+                when (res.code) {
+                    Code.SUCCESS -> call.respond(HttpStatusCode.OK, ReverseTextResponse(reverseText))
+                    Code.ASSUMED_FAIL -> call.respond(HttpStatusCode.BadRequest, ReverseTextErrorResponse(res.message))
+                    Code.UNKNOWN_FAIL -> call.respond(
+                        HttpStatusCode.InternalServerError,
+                        ReverseTextErrorResponse(res.message)
+                    )
+                }
+            }
+        }
+    }
+    server.start(wait = true)
+}
